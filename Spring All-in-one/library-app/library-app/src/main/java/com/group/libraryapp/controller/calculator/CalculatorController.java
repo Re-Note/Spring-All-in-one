5822bf3bd4b3cf353d6d701163c84c5cb2323f94
Re-Note <dldydtn207@gmail.com>
2024-05-01 15:21:01 +0900

    step6

--- Spring All-in-one/library-app/library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ Spring All-in-one/library-app/library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,8 +1,10 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 // ctrl + alt + o import 문 정리
@@ -14,4 +16,10 @@ public class CalculatorController {
     public int addTwoNumbers( CalculatorAddRequest request ) {
         return request.getNumber1() + request.getNumber2();
     }
+
+    @PostMapping("/multiply") // POST /multiply
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request ) {
+        return request.getNumber1() * request.getNumber2();
+    }
+
 }

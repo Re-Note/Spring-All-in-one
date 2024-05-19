1ae91e9b8cd148eaa3a8194e8877f2682b4f330b
Re-Note <dldydtn207@gmail.com>
2024-05-19 19:56:38 +0900

    step32

--- Spring All-in-one/library-app/library-app/src/main/java/com/group/libraryapp/service/book/BookService.java
+++ Spring All-in-one/library-app/library-app/src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -8,6 +8,7 @@ import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository;
 import com.group.libraryapp.dto.book.request.BookCreateRequest;
 import com.group.libraryapp.dto.book.request.BookLoanRequest;
+import com.group.libraryapp.dto.book.request.BookReturnRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -51,7 +52,18 @@ public class BookService {
                 .orElseThrow(IllegalArgumentException::new);
 
         // 5. 유저 정보와 책 정보를 기반으로 UserLoanHistory를 저장한다.
-        userLoanHistoryRepository.save(new UserLoanHistory(user.getId(), book.getName()));
+        userLoanHistoryRepository.save(new UserLoanHistory(user, book.getName()));
 
+    }
+
+    @Transactional
+    public void returnBook(BookReturnRequest request){
+        // 유저 정보를 가져온다
+        User user = userRepository.findByName(request.getUserName())
+                .orElseThrow(IllegalArgumentException::new);
+
+        UserLoanHistory history = userLoanHistoryRepository.findByUserIdAndBookName(user.getId(), request.getBookName())
+                .orElseThrow(IllegalArgumentException::new);
+        history.doReturn();
     }
 }
